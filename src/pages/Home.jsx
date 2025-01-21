25113bef9735bf410a1b8414f384555c81597d4a
Zenkai92 <aschierimarc@gmail.com>
2025-01-21 11:51:29 +0100

    Mise en place de la page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router';
+import { Login, Logout } from "../auth/LogInOut";
+import { useAuth } from "../auth/AuthProvider";
+
+export default function Home() {
+    const { user } = useAuth();
+    return (
+        <div>
+            <Link to = "#">
+                <button>Voir les produits</button>
+            </Link>
+            <Link to = "#">
+                <button>Pour ne pas nous contacter</button>
+            </Link>
+               {user ? (
+                <>Bienvenue {user.username}
+                <Logout/>
+                </>
+               ):(
+                <Login/>
+               )}
+        </div>
+    );
+}
